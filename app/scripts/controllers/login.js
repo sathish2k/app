54d64de8447e80f7d3dc335cdabf0d2f1f9fb8e8
satsathish2k <you@example.com>
2016-12-31 13:21:55 +0530

    g

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -318,7 +318,7 @@ angular.module('app')
             }else if(toState.data.isloggedin == 'true'&&!$localStorage.token) {
             console.log("UN AUTHORIZED!!!");
              event.preventDefault();
-            $location.path('/home');
+            $location.path('/signup');
             }
              $rootScope.id=$localStorage.id;
               
